8f7f85d1d9461fabbdf5ddb4e97a375f704040ab
anurag <anuragnbr@gmail.com>
2022-12-05 01:11:17 +0530

    student app

---
+++ student.js
@@ -0,0 +1,59 @@
+
+
+function register()
+{
+    username=reg_name.value,
+    phnumber=reg_num.value,
+    gmail=reg_mail.value,
+    pswd=reg_pswd.value,
+    qualification=reg_qual.value
+
+    studRegister={
+        username,
+        phnumber,
+        gmail,
+        pswd,
+        qualification
+    }
+
+    if(gmail in localStorage)
+    {
+        alert('account already exist')
+    }
+    else
+    {
+        localStorage.setItem(gmail,JSON.stringify(studRegister))
+        alert('registration successful')
+        window.location.href='index.html'
+    }
+
+}
+
+function login()
+{
+     Gmail=log_mail.value,
+     pass=log_pswd.value
+
+
+        if(Gmail in localStorage)
+        {
+            studDetails=JSON.parse(localStorage.getItem(Gmail))
+            
+            if(pass==studDetails.pswd)
+            {
+                alert('login successfull')
+                window.location.href='home.html'
+            }
+            else
+            {
+                alert('incorrect password')
+            }
+        }
+        else
+        {
+            alert('invalid account')
+        }
+
+
+     
+}
